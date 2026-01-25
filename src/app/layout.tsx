750e4d90fad7d7f366658029218e39b2fbf91773
Averton Silva Dias <verton3@gmail.com>
2026-01-25 16:29:39 +0000

    ainda não tenho a pasta android no projeto e é apenas o pwa mesmo, isso

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import '@/lib/firebase'; // Garante a inicialização do Firebase no início
 import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
