24af40ce6d63526a614e347b34c462cbcfc539de
Averton Silva Dias <verton3@gmail.com>
2026-02-01 15:41:34 +0000

    na verdade ao fazer o login com o google a tela que pede para escolher q

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,3 @@
-
 import { initializeApp, getApp, getApps, type FirebaseApp } from "firebase/app";
 import { getAuth, type Auth } from "firebase/auth";
 import { 
@@ -9,8 +8,9 @@ import {
 const firebaseConfig = {
   apiKey: "AIzaSyDiKZq6bOkazeGAbh-bpjePrOeT5EhPX_0",
   authDomain: "gutters-budget-pro.firebaseapp.com",
+  databaseURL: "https://gutters-budget-pro-default-rtdb.firebaseio.com",
   projectId: "gutters-budget-pro",
-  storageBucket: "gutters-budget-pro.appspot.com",
+  storageBucket: "gutters-budget-pro.firebasestorage.app",
   messagingSenderId: "766057124102",
   appId: "1:766057124102:web:a8b2ed8d064964e4980e87"
 };
